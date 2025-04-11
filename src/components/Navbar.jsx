d2b16f36fc624d427e1795ab83096e93ce41a986
kartiklomte <166540914+kartiklomte@users.noreply.github.com>
2025-04-12 00:47:57 +0530

    navbar width bug solve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='absolute flex flex-row gap-4 place-content-center bg-amber-200 p-2 top-0 w-full mb-2'>
+    <div className='fixed flex flex-row gap-4 place-content-center bg-amber-200 p-2 top-0 left-0 right-0 mb-2 z-50'>
       <NavLink to="/" className="border border-blue-800 rounded-md pl-2 pr-2">
         Home
       </NavLink>
